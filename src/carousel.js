52ea725a06b1e9bf11dfd2679bb8a8b6c8be66a7
a_osovsky <116560582+ArtemyOsovskoi@users.noreply.github.com>
2024-09-20 17:27:46 +0300

    carousel first draft, positioning

--- src/carousel.js
+++ src/carousel.js
@@ -4,17 +4,13 @@ console.log(getImages.childNodes);
 
 export function previous() {
   let previousButton = document.getElementById("previousButton");
-  /* Нужно взять изображения (слайды, див - один слайд)
-        И проходится по ним циклом(?) который будет итерировать
-        Нажатие кнопки проходит по массиву слайдов
-        Находит текущий слайд, определяет какой идет за ним или до него
-        И выводит на место текущего нужный слайд
-        Т.е. меняет див, манипулируя DOMом.
-    */
+
 
   previousButton.addEventListener("click", () => {
-   /*  let targetNode = getImages[1].previousSibling;
-    targetNode.style.position = 'absolute'; */
+    let targetNode = getImages.childNodes[1];
+    targetNode.style.position = 'absolute';
+    targetNode.style.left = '0';
+    targetNode.style.top = '0';
     
   });
 
@@ -24,36 +20,12 @@ export function next() {
     let nextButton = document.getElementById("nextButton");
 
     nextButton.addEventListener("click", () => {
-    /*     for (let index = 0; index <= 9; index++) {
-            const targetNode = getImages.childNodes[index];
-            console.log(targetNode);
-            let nextNode = targetNode.nextSibling;
-            console.log(nextNode);
-            //console.log(nextNode);
-            nextNode.style.position = 'absolute';  
-        } */
-
-            //getImages.style.position = 'absolute';
-            /* getImages.style.left = '100px';
-            getImages.style.top = '200px'; */
+      
         
-        //let targetNode = getImages.childNodes[0];
-        
-        /* secondChild.style.left = '100px';
-        secondChild.style.top = '100px'; */
+        let targetNode = getImages.childNodes[3];
+        targetNode.style.position = 'absolute';
+        targetNode.style.left = '0';
+        targetNode.style.top = '0';
 
     });
 }
-
-        //и так это работает частично, я могу указывать номер нода
-        //и он встает на нужное место в центр
-        //теперь нужно понять как динамично выбирать следующий нод и ставить
-        //как абсолют позишн именно его - следующий нод (а не вручную цифру писать)
-        //или все через display лучше? а не позишн
-
-/* 
-по сути мне надо менять позиции 4х дивов
-как слайды они идут от одного к другому т.е. нужно тригерить характеристику
-которая отвечает за позиционирование дива в маленьком рамочном диве
-
-*/
