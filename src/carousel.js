b5da7cdb55f05769ae1dd62df4e0764757aa124d
a_osovsky <116560582+ArtemyOsovskoi@users.noreply.github.com>
2024-09-25 15:34:18 +0300

    working next function: change flexbox order property of container div to move images inside those containers

--- src/carousel.js
+++ src/carousel.js
@@ -1,31 +1,46 @@
-let getImages = document.getElementById("images");
-console.log(getImages);
-console.log(getImages.childNodes);
+let getImage = document.getElementById("images");
+let getImageNodes = getImage.childNodes;
+
+let firstImage = document.getElementById("cont_one");
+let secondImage = document.getElementById("cont_two");
+let thirdImage = document.getElementById("cont_three");
+let fourthImage = document.getElementById("cont_four");
+firstImage.style.order = "1";
+secondImage.style.order = "2";
+thirdImage.style.order = "3";
+fourthImage.style.order = "4";
+
+console.log(getImage);
+console.log(getImage.childNodes);
 
 export function previous() {
   let previousButton = document.getElementById("previousButton");
 
-
   previousButton.addEventListener("click", () => {
-    let targetNode = getImages.childNodes[1];
-    targetNode.style.position = 'absolute';
-    targetNode.style.left = '0';
-    targetNode.style.top = '0';
-    
+    let targetNode = getImageNodes[1];
   });
-
 }
 
 export function next() {
-    let nextButton = document.getElementById("nextButton");
-
-    nextButton.addEventListener("click", () => {
-      
-        
-        let targetNode = getImages.childNodes[3];
-        targetNode.style.position = 'absolute';
-        targetNode.style.left = '0';
-        targetNode.style.top = '0';
-
-    });
+  let nextButton = document.getElementById("nextButton");
+  nextButton.addEventListener("click", () => {
+    if (firstImage.style.order == "1") {
+      firstImage.style.order = "2";
+      secondImage.style.order = "1";
+    } else if (secondImage.style.order == "1") {
+      secondImage.style.order = "2";
+      thirdImage.style.order = "1";
+      firstImage.style.order = "3";
+    } else if (thirdImage.style.order == "1") {
+      thirdImage.style.order = "2";
+      fourthImage.style.order = "1";
+      secondImage.style.order = "4";
+      firstImage.style.order = "3";
+    } else if (fourthImage.style.order == "1") {
+      firstImage.style.order = "1";
+      secondImage.style.order = "2";
+      thirdImage.style.order = "3";
+      fourthImage.style.order = "4";
+    }
+  });
 }
